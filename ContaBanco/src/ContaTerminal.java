b148b5070eef73541ad2d5ca3685339b03b1802a
Lucas Birlem <lucase.birlem@gmail.com>
2024-05-17 09:58:15 -0300

    Projeto conta banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu número da conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo  + " já está disponível para saque.");
+    }
+}
